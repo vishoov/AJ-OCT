21435f2d4eb9804a169b4581c3952446ef4fa082
Vishoo <vverma971@gmail.com>
2025-10-23 23:01:09 +0530

    user management api

--- User Management API/model/user.model.js
+++ User Management API/model/user.model.js
@@ -3,22 +3,43 @@ const mongoose = require('mongoose');
 
 const userSchema = mongoose.Schema({
     // name,
-    name:{
+    username:{
         type:String, //this is the type of the data 
         required:true, //this checks if this data is compulsary or not
+        //smallcase, uppercase or number -> alpha-numeric type of data 
+        validate:{
+            validator:function(arg){
+                return /^[a-zA-Z0-9]+$/.test(arg);
+            },
+            message: props => `${props.value} is not a valid username`
+        },
+        unique:true
     },
     email:{
         type:String,
         required:true,
         unique:true, //this avoids duplicacy of emails 
+        match:/^((?!\.)[\w\-_.]*[^.])(@\w+)(\.\w+(\.\w+)?[^.\W])$/ 
     },
     password:{
         type:String,
-        required:true
+        required:true,
+        minLength:[8,'This is a custom error written by me, that says please enter a longer password'],
+        maxLength:20,
+        match:/^(?=.*\d)(?=.*[A-Z])(?=.*[a-z])(?=.*[^\w\d\s:])([^\s]){8,16}$/,
+        select:true //it will not send password whenever we fetch any user data from the DB
     },
     role:{
         type:String,
         enum:['user', 'guest', 'admin', 'superadmin']
+    },
+    age:{
+        //only for adults and for people less than 60 years of age, compulsary 
+        type:Number,
+        min:18,
+        max:60,
+        required:[true, "Without age, you cannot signup"]
+
     }
 // email,
 // password,
--- User Management API/view/user.routes.js
+++ User Management API/view/user.routes.js
@@ -16,7 +16,12 @@ router.post("/register", async (req, res)=>{
     const data = req.body;
 
     // users.push(data);
-    const userData = await User.create(data);
+    //mongoose way
+    // const userData = await User.create(data);
+
+    //OOP way
+    const userData = new User(data);
+    await userData.save();
 
     res.json({
         message:"User created",
@@ -24,13 +29,15 @@ router.post("/register", async (req, res)=>{
     })
 })
 //login
-router.post('/login', (req, res)=>{
+router.post('/login', async (req, res)=>{
+    try{
     const { email, password } = req.body;
 
-    const data = users.find((user)=>{
-        return email === user.email
-    })
+    const data= await User.findOne({
+            email:email
+        })
 
+console.log(data)
 
     if(data.password!==password){
         return res.status(400).send("Password is incorrect")
@@ -41,22 +48,182 @@ router.post('/login', (req, res)=>{
         message:"You have successfully logged in",
         data
     })
+    }
+    catch(err){
+        return res.send(err.message)
+    }
 })
+
+// router.get("/getByUsername/:username", )
+//username 
+
 
 //users/
-router.get("/users", (req, res)=>{
+router.get("/users", async (req, res)=>{
+    try{
+    const users = await User.find();
 
+    res.json(
+        {
+            users
+        }
+    )
+}
+catch(err){
+    return res.status(400).send(err.message)
+}
 })
+
+
 // /users/:id
-router.get("/users/:id", (req, res)=>{
+router.get("/users/id", async (req, res)=>{
+    const { id } = req.query;
+
+    // const user = await User.findOne({
+    //     _id:id
+    // })
+
+    //quick indexing 
+
+    const user = await User.findById(id)
+
+    if(!user){
+        return res.status(404).send("User not found");
+    }
+
+    res.status(200).json({
+        user
+    })
 
 })
+
+
+
 //DELETE users/:id
-router.delete("/users/:id", (req, res)=>{
+router.delete("/users/:id", async (req, res)=>{
+    
+    const user = await User.findByIdAndDelete(req.params.id);
+
+    res.json({
+        user,
+        message:"User deleted successfully"
+    })
 
 })
 
 
+
+
+//adult users (age>18)
+router.get("/adultUsers", async (req, res)=>{
+    try{
+        const users = await User.find({
+            age: {
+                $gt : 18
+            }
+        })
+
+        res.json(
+            users
+        )
+    }
+    catch(err){
+        return res.send(err.message)
+    }
+})
+
+router.get("/userswithadmin", async (req, res)=>{
+    try{
+        const users = await User.find(
+            {
+                role: {
+                    $in: ["user"]
+                }
+            }
+        )
+
+        res.json(users)
+    }
+    catch(err){
+        return res.status(400).send(err.message)
+    }
+})
+
+router.get('/userfifty', async (req, res)=>{
+    try{
+        // const age = 30 
+        // const role = 'admin';
+        const users = await User.find(
+            {
+                $and :[
+                    { age: 50 },
+                    { role:'user' }
+                ]
+            }
+        )
+        return res.json(users)
+    }
+    catch(err){
+        return res.status(400).send(err.message)
+    }
+})
+
+router.put('/updateOne', async (req, res)=>{
+    try{
+        const { username, newEmail } = req.body;
+
+        const updatedUser = await User.findOneAndUpdate(
+            //1. filtering condition 
+            {
+                username:username
+            },
+            //2. add the new info
+            {
+                $set:{
+                    email:newEmail
+                }
+            },
+            {
+                new:true //this will send the updated document 
+            }
+        )
+        return res.json(updatedUser)
+    }
+    catch(err){
+        return res.json(err.message)
+    }
+})
+
+router.put("/findById/:id", async (req, res)=>{
+    const id = req.params.id;
+
+    const user = await User.findByIdAndUpdate(
+        id,
+        {
+            role:"admin"
+        },
+        {
+            new:true
+        }
+    )
+
+    res.json(user)
+})
+
+router.delete('/deletebyemail', async (req, res)=>{
+    const email = req.body.email;
+
+    const userDeleted = await User.deleteOne(
+        {
+            email:email
+        }
+    )
+
+    res.json({
+        message:"User deleted",
+        userDeleted
+    })
+})
 
 
 module.exports=router;
